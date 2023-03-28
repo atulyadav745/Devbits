4c5e242e7a2c3a36086ee7c4d5a2536f0d8c682c
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-03-29 00:42:18 +0530

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
+import "./css/style.css";
 
-import './css/style.css';
-
-import './charts/ChartjsConfig';
+import "./charts/ChartjsConfig";
 
 // Import pages
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/SignUp/Signup";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
-    
       <Routes>
         <Route index element={<Home />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
-
+    </>
   );
 }
 
