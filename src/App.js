02ba3bee601321011e43ea71045bc0b629831177
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-03-28 22:47:57 +0530

    made login and signup page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/SignUp/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Home/>} />
-
+        <Route index element={<Home />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
